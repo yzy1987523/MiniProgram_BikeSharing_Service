53a6c97e6d56f36640d1b3fcbe0b2bbbdc014ad1
迟谋 <705359976@qq.com>
2019-04-15 23:08:23 +0800

    sms
    腾讯云依赖+redis依赖

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -1,17 +1,21 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.example.service.UserService;
+
 @Controller
 public class UserController {//控制层主要接受用户请求并响应，业务层主要处理具体业务，反馈用户操作的结果
-	private UserService 
-	
+	@Autowired//按类型注入，一般是要用到其他类时的一种引用格式
+	private UserService userService;
 	
 	@RequestMapping("/user/genCode")
 	@ResponseBody
 	public boolean genVerifyCode(String countryCode,String phoneNum) {
-		return false;
+		boolean flag=userService.sendMsg(countryCode,phoneNum);
+		return flag;
 	}
 }
--- src/main/java/com/example/service/UserServiceImpl.java
+++ src/main/java/com/example/service/UserServiceImpl.java
@@ -1,17 +1,35 @@
 package com.example.service;
 
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import com.github.qcloudsms.SmsSingleSender;
 
 @Service
 public class UserServiceImpl implements UserService {
-
+	@Autowired
+	private StringRedisTemplate stringRedisTemplate;
 	@Override
 	public boolean sendMsg(String countryCode, String phoneNum) {
+		boolean flag=true;
 		// 调用腾讯云的短信API
+		int appid=Integer.parseInt(stringRedisTemplate.opsForValue().get("appid"));//从redis中读取appid（appid要提前set）
+		String appkey=stringRedisTemplate.opsForValue().get("appkey");
 		//生成随机数字
-		//发送短信
-		//将手机号+验证码存到redis
-		return false;
+		String code=(int)(Math.random()*9+1)*1000+"";
+		int limitTime=300;
+		SmsSingleSender ssender=new SmsSingleSender(appid, appkey); 
+		try {
+			//0表示普通短信6
+			ssender.send(0, countryCode, phoneNum, code+"是您的验证码，"+limitTime+"秒内有效", "","");
+			stringRedisTemplate.opsForValue().set(phoneNum,code,300,TimeUnit.SECONDS);//往redis中存入临时数据
+		}catch(Exception e) {
+			flag=false;
+			e.printStackTrace();
+		}
+		return flag;
 	}
 
 }
